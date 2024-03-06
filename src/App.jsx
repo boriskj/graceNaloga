7449f4243435750631b999480e8b29ca2ea4d290
boriskj <boris_krj@protonmail.com>
2024-03-06 17:57:38 +0100

    qoute naloga znackeimena

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Badge } from "@/components/ui/badge";
 export default function App() {
   const [quotes, setQuotes] = useState([]);
   const [tags, setTags] = useState(["tag1", "tag2", "tag3", "tag4"]);
+  const[selectedTag, setSelectedTag]= useState(null)
 
   async function getQuotes() {
     const request = await fetch(`/quotes.json`);
@@ -19,21 +20,47 @@ export default function App() {
   }, []);
 
   useEffect(() => {
-    console.log(quotes);
+
+    //Ali qutes obstaja in ima elemente
+    if(!(quotes && quotes.length> 0)){
+      // Ce ne , prenehamo izvajati funkcijo
+      return;
+     }
+     //Ce pridemo do sem , vemo da se zgornji if ni izvedel
+     let tags = [];
+
+     quotes.forEach((e) => {
+      const quote_tags = e["tags"];
+      // 1. for loop za vse tage od tega quota (e["tags"]);
+      quote_tags.forEach((t) => {
+        console.log(t)
+        if (tags.includes(t)){
+          console.log(t)
+        }
+        else{
+          tags.push(t)                  
+        }
+
+      })})
+      // 2. za vsakega preverimo,ce je v seznamu tags.includes("FamuoQuotes")
+      // 3. ce tega elementa se ni v seznamu, ga dodamo tags.push("FamousQuotes")
+
+     setTags(tags);
+     console.log(tags)
   }, [quotes]);
 
   return (
     <div className="p-4">
       <div>
         {tags.map((tag) => (
-          <Badge> {tag}</Badge>
+          <Badge key={tag}>{tag}</Badge>
         ))}
       </div>
       <div className="grid grid-cols-3 gap-4">
         {quotes.map((quote) => (
-          <Quote test={quote}></Quote>
+          <Quote test={quote} key = {quote["id"]}></Quote>
         ))}
       </div>
     </div>
   );
-}
+  }
