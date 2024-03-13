3b1c88bee2116e1ccd23366658a07f5019c32c48
boriskj <boris_krj@protonmail.com>
2024-03-13 17:54:53 +0100

    znacke za tage

--- src/App.jsx
+++ src/App.jsx
@@ -6,61 +6,83 @@ import { Badge } from "@/components/ui/badge";
 export default function App() {
   const [quotes, setQuotes] = useState([]);
   const [tags, setTags] = useState(["tag1", "tag2", "tag3", "tag4"]);
-  const[selectedTag, setSelectedTag]= useState(null)
+  const [selectedTag, setSelectedTag] = useState(null);
 
   async function getQuotes() {
     const request = await fetch(`/quotes.json`);
     const podatki = await request.json();
-
     setQuotes(podatki);
   }
+
+  function isQuoteSelected(quote) {
+    if (!selectedTag) return true;
+    return quote.tags.includes(selectedTag);
+    // quote.tags.forEach((tag) => {
+    //   if (tag == selectedTag) {
+    //     console.log(tag + " " + selectedTag);
+    //     return true;
+    //   }
+    // });
+    // return false;
+  }
+
+  useEffect(() => {
+    console.log("quotes:", quotes);
+    console.log(
+      "filtered:",
+      quotes.filter((q) => isQuoteSelected(q)),
+    );
+  }, [selectedTag, quotes]);
 
   useEffect(() => {
     getQuotes();
   }, []);
 
   useEffect(() => {
-
     //Ali qutes obstaja in ima elemente
-    if(!(quotes && quotes.length> 0)){
+    if (!(quotes && quotes.length > 0)) {
       // Ce ne , prenehamo izvajati funkcijo
       return;
-     }
-     //Ce pridemo do sem , vemo da se zgornji if ni izvedel
-     let tags = [];
+    }
+    //Ce pridemo do sem , vemo da se zgornji if ni izvedel
+    let tags = [];
 
-     quotes.forEach((e) => {
+    quotes.forEach((e) => {
       const quote_tags = e["tags"];
       // 1. for loop za vse tage od tega quota (e["tags"]);
       quote_tags.forEach((t) => {
-        console.log(t)
-        if (tags.includes(t)){
-          console.log(t)
+        //console.log(t);
+        if (tags.includes(t)) {
+          //console.log(t);
+        } else {
+          tags.push(t);
         }
-        else{
-          tags.push(t)                  
-        }
+      });
+    });
+    // 2. za vsakega preverimo,ce je v seznamu tags.includes("FamuoQuotes")
+    // 3. ce tega elementa se ni v seznamu, ga dodamo tags.push("FamousQuotes")
 
-      })})
-      // 2. za vsakega preverimo,ce je v seznamu tags.includes("FamuoQuotes")
-      // 3. ce tega elementa se ni v seznamu, ga dodamo tags.push("FamousQuotes")
-
-     setTags(tags);
-     console.log(tags)
+    setTags(tags);
+    console.log(tags);
   }, [quotes]);
 
   return (
     <div className="p-4">
+      <h2>{selectedTag}</h2>
       <div>
         {tags.map((tag) => (
-          <Badge key={tag}>{tag}</Badge>
+          <Badge key={tag} onClick={() => setSelectedTag(tag)}>
+            {tag}
+          </Badge>
         ))}
       </div>
       <div className="grid grid-cols-3 gap-4">
-        {quotes.map((quote) => (
-          <Quote test={quote} key = {quote["id"]}></Quote>
-        ))}
+        {quotes
+          .filter((q) => isQuoteSelected(q))
+          .map((quote) => (
+            <Quote test={quote} key={quote["id"]}></Quote>
+          ))}
       </div>
     </div>
   );
-  }
+}
